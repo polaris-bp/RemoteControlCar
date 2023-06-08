db63b4ceb62f85293bd54ff2ff32b1d92287cf5b
Taichi Uragami <tu.0514.0315@icloud.com>
2023-06-08 21:40:15 +0900

    Initial Commit

---
+++ RemoteControlCar/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  RemoteControlCar
+//
+//  Created by Taichi Uragami on 2023/06/08.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
